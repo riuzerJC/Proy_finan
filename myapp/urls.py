8bd536fcdc7eced6a0dd37ef84fce5c2f7998d05
RayCV <rayjc.desing@gmail.com>
2023-05-29 22:43:23 -0500

    agregando actualizacion

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello)
+    path('', views.index),
+    path('', views.hello),
+    path('hello/<str:username>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/', views.tasks)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Task
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 
-def hello(request):
-    return HttpResponse("Hola mundo")
+def index(request):
+    title = 'Django course'
+    return render(request, 'index.html')
+
+
+def hello(request, username):
+    return HttpResponse("<h2>Hello %s </h2>" % username)
+
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return render(request, 'projects.html')
+
+
+def tasks(request):
+    # task = get_object_or_404(Task, id=id)
+    return render(request, 'task.html')
